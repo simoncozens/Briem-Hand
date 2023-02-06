c6cacee7f0d5578c90c4c09cab4c8ece5cafd5cc
Simon Cozens <simon@simon-cozens.org>
2023-02-06 09:22:36 +0000

    Fix paths

--- scripts/add-color.py
+++ scripts/add-color.py
@@ -3,7 +3,7 @@ from fontTools.ttLib import TTFont
 from fontTools.fontBuilder import addFvar
 from fontTools.ttLib.tables._f_v_a_r import Axis
 
-font = TTFont("../fonts/variable/BriemHand[wght].ttf")
+font = TTFont("fonts/variable/BriemHand[wght].ttf")
 
 print("Adding axes...")
 
@@ -19,8 +19,8 @@ for instance in font["fvar"].instances:
 	instance.coordinates["GDLN"] = 0 # ???
 
 print("Adding paints...")
-compile_paints(font, open("paints.py").read())
+compile_paints(font, open("scripts/paints.py").read())
 
 print("Saving...")
-font.save("../fonts/variable/BriemHand[wght,GDLN].ttf")
-print("../fonts/variable/BriemHand[wght,GDLN].ttf")
+font.save("fonts/variable/BriemHand[wght,GDLN].ttf")
+print("fonts/variable/BriemHand[wght,GDLN].ttf")
